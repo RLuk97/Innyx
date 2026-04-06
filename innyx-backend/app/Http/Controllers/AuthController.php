be46f7480b2d6d05e452c1fbdbd0ce2a59761eb6
Ryan Lucas <127062432+RLuk97@users.noreply.github.com>
2026-04-05 22:01:38 -0400

    Update AuthController.php

--- innyx-backend/app/Http/Controllers/AuthController.php
+++ innyx-backend/app/Http/Controllers/AuthController.php
@@ -10,24 +10,19 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        // 1. Valida os dados de entrada
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        // 2. Tenta autenticar
         if (!Auth::attempt($credentials)) {
             return response()->json([
                 'message' => 'Credenciais inválidas'
             ], 401);
         }
-
-        // 3. Busca o usuário e gera o Token
         $user = User::where('email', $request->email)->firstOrFail();
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        // 4. Retorna o Token e o tipo
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',

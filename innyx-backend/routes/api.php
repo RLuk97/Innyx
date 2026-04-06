cb5faa4771e33baf0a8ff28723a3077bec9d8c07
Ryan Lucas <127062432+RLuk97@users.noreply.github.com>
2026-04-05 23:00:11 -0400

    Update api.php

--- innyx-backend/routes/api.php
+++ innyx-backend/routes/api.php
@@ -4,10 +4,8 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Rota pública para login (onde o Front vai buscar o token)
 Route::post('/login', [AuthController::class, 'login']);
 
-// Rotas protegidas (exigem Token Bearer)
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('products', ProductController::class);
 });

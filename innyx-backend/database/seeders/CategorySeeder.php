f79013f061ad100ec43ee8ed81823050170919f1
Ryan Lucas <127062432+RLuk97@users.noreply.github.com>
2026-04-05 22:55:42 -0400

    Update CategorySeeder.php

--- innyx-backend/database/seeders/CategorySeeder.php
+++ innyx-backend/database/seeders/CategorySeeder.php
@@ -7,14 +7,8 @@ use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        // Criando as categorias base para o sistema (ID fixo para sincronizar com o Vue)
         Category::create(['id' => 1, 'name' => 'Hardware']);
         Category::create(['id' => 2, 'name' => 'Software']);
         Category::create(['id' => 3, 'name' => 'Serviços']);
